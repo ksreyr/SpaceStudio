9ac046f270dd7a2c7fda249c0af5c65eae265f69
Liam <liam@contra-bit.com>
2020-05-12 13:35:09 +0200

    player and ships

---
+++ api.space/src/main/java/org/spacestudio/resources/ShipResource.java
@@ -0,0 +1,44 @@
+package org.spacestudio.resources;
+
+import org.spacestudio.openapi.space.model.Ship;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+@Path("/ships")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ShipResource {
+
+    private Set<Ship> ships = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public ShipResource() {
+        ships.add(new Ship("Flying Snowstorm", "Winter ship"));
+        ships.add(new Ship("Pineapple Express", "Tropical ship"));
+    }
+
+    @GET
+    public Set<Ship> list() {
+        return ships;
+    }
+
+    @POST
+    public Set<Ship> add(Ship ship) {
+        ships.add(ship);
+        return ships;
+    }
+
+    @DELETE
+    public Set<Ship> delete(Ship ship) {
+        ships.removeIf(existingShip -> existingShip.getName().contentEquals(ship.getName()));
+        return ships;
+    }
+}
