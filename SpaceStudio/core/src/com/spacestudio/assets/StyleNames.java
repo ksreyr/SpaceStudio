7a28c1d61485ad09d2a8b6e0349797f7d3f5457a
clara <odinius@uni-bremen.de>
2020-05-14 11:11:02 +0200

    add Prototyp for Screens, Loading Screen, Menu Screen

---
+++ SpaceStudio/core/src/com/spacestudio/assets/StyleNames.java
@@ -0,0 +1,14 @@
+package com.spacestudio.assets;
+
+public class StyleNames {
+
+
+    public static final String EMPHASISCOLORED = "emphasis-colored";
+    public static final String EMPHASIS = "emphasis";
+    public static final String EMPHASISTEXTBUTTON = "emphasis-textbutton";
+    public static final String TITLELABEL = "title-label";
+
+
+    private StyleNames() {
+    }
+}
---
+++ SpaceStudio/core/src/com/spacestudio/screen/universe/UniverseScreen.java
@@ -0,0 +1,4 @@
+package com.spacestudio.screen.universe;
+
+public class UniverseScreen {
+}
