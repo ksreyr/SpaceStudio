749e71c1692434794f13479fd8fa5771f84de3b5
Liam <liam@contra-bit.com>
2020-05-31 13:04:04 +0200

    server runs

--- Server/src/main/java/de/spaceStudio/server/model/Ressource.java
+++ Server/src/main/java/de/spaceStudio/server/model/Ressource.java
@@ -12,4 +12,6 @@ public class Ressource {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    private String name;
+
 }
--- Server/src/main/java/de/spaceStudio/server/repository/ShopItemsRepository.java
+++ Server/src/main/java/de/spaceStudio/server/repository/ShopItemsRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ShopItemsRepository extends JpaRepository<ShopItem, Integer> {
-    Optional<ShopItem> findByName(String name);
 }
