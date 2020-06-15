a0fc4f28f593db9d3708fe7c42a45b347caf9ac1
Miguel Caceres <mcaceres@uni-bremen.de>
2020-06-15 16:08:19 +0200

    Add Junit Test to test REST API and refactoring code

--- Server/src/main/java/de/spaceStudio/server/controller/StartPageController.java
+++ Server/src/main/java/de/spaceStudio/server/controller/StartPageController.java
@@ -3,10 +3,12 @@ package de.spaceStudio.server.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static de.spaceStudio.server.utils.ServerUrlsPath.HOME;
+
 @RestController
 public class StartPageController {
 
-    @RequestMapping("/")
+    @RequestMapping(HOME)
     public String startPage(){
         return "Spacestudio Server is running...";
     }
