33e12e684548ca0f8943dcebea1daae31d8c6c55
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-06-19 15:38:23 +0200

    updtae

---
+++ Server/src/main/java/de/spaceStudio/server/controller/CrewMemberControllerImpl.java
@@ -0,0 +1,66 @@
+package de.bremen.server.controller;
+
+import de.bremen.server.model.CrewMember;
+import de.bremen.server.model.Section;
+import de.bremen.server.repository.CrewMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+public class CrewMemberControllerImpl implements CrewMemberController {
+    @Autowired
+    CrewMemberRepository crewMemberRepository;
+
+    @Override
+    @RequestMapping(value = "/crewmembers", method = RequestMethod.GET)
+    public List<CrewMember> getAllCrewMembers() {
+        return crewMemberRepository.findAll();
+    }
+
+    @Override
+    @RequestMapping(value = "/crewmember/{id}", method = RequestMethod.GET)
+    public CrewMember getCrewMember(@PathVariable Integer id) {
+        return crewMemberRepository.getOne(id);
+    }
+
+    @Override
+    @RequestMapping(value = "/crewmember", method = RequestMethod.POST)
+    public String addCrewMember(@RequestBody CrewMember crewMember) {
+        crewMemberRepository.save(crewMember);
+        return HttpStatus.OK.toString();
+    }
+
+    @Override
+    @RequestMapping(value = "/crewmember", method = RequestMethod.PUT)
+    public CrewMember updateCrewMember(@RequestBody CrewMember crewMember) {
+        CrewMember crewMemberToUpdate= crewMemberRepository
+                .getOne(crewMember.getId());
+        crewMemberToUpdate = crewMember;
+        crewMemberRepository.save(crewMemberToUpdate);
+        return crewMemberToUpdate;
+    }
+
+    @Override
+    @RequestMapping(value = "/section/{id}", method = RequestMethod.DELETE)
+    public String deleteCrewMemberById(@PathVariable Integer id) {
+        crewMemberRepository.deleteById(id);
+        return HttpStatus.OK.toString();
+    }
+
+    @Override
+    @RequestMapping(value = "/section", method = RequestMethod.DELETE)
+    public String deleteAllCrewMembers() {
+        crewMemberRepository.deleteAll();
+        return HttpStatus.OK.toString();
+    }
+
+    @Override
+    public CrewMember updatePostion(int shipID, CrewMember crewMember, Section sectionNew, Section sectionOld) {
+        return null;
+    }
+}
---
+++ Server/src/main/java/de/spaceStudio/server/controller/StationControllerImpl.java
@@ -0,0 +1,65 @@
+package de.bremen.server.controller;
+
+import de.bremen.server.model.Station;
+import de.bremen.server.repository.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+public class StationControllerImpl implements StationController{
+    @Autowired
+    StationRepository stationRepository;
+
+    @Override
+    @RequestMapping(value = "/station", method = RequestMethod.GET)
+    public List<Station> getAllStations() {
+        return stationRepository.findAll();
+    }
+
+    @Override
+    @RequestMapping(value = "/station/{id}", method = RequestMethod.GET)
+    public Station getStation(@PathVariable Integer id) {
+        return stationRepository.getOne(id);
+    }
+
+    @Override
+    @RequestMapping(value = "/station", method = RequestMethod.POST)
+    public String addStation(@RequestBody Station station) {
+        stationRepository.save(station);
+        return HttpStatus.OK.toString();
+    }
+
+    @Override
+    @RequestMapping(value = "/station", method = RequestMethod.PUT)
+    public Station updateStation(@RequestBody Station station) {
+        Station stationToUpdate= stationRepository
+                .findById(station.getId()).get();
+        stationToUpdate=station;
+        stationRepository.save(stationToUpdate);
+        return stationToUpdate;
+    }
+
+    @Override
+    @RequestMapping(value = "/station/{id}", method = RequestMethod.DELETE)
+    public String deleteStationById(@PathVariable Integer id) {
+        stationRepository.deleteById(id);
+        return HttpStatus.ACCEPTED.toString();
+    }
+
+    @Override
+    @RequestMapping(value = "/section", method = RequestMethod.DELETE)
+    public String deleteAllStations() {
+        stationRepository.deleteAll();
+        return HttpStatus.OK.toString();
+    }
+
+    @Override
+    public String hashPassword(String weakPassword) {
+        return null;
+    }
+}
