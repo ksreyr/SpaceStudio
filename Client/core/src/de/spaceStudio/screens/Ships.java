381231d88839fc22852c1960a997e9d086e82b82
Mehmet Ali Baykara <baykara@uni-bremen.de>
2020-06-20 18:42:10 +0200

    Add ship rooms

--- Client/core/src/de/spaceStudio/screens/Ships.java
+++ Client/core/src/de/spaceStudio/screens/Ships.java
@@ -11,34 +11,48 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import de.spaceStudio.MainClient;
+import de.spaceStudio.server.model.Player;
+import de.spaceStudio.server.model.Ship;
+import java.util.ArrayList;
+import de.spaceStudio.server.controller.ShipControllerImpl;
+
 
 //“Sound effects obtained from https://www.zapsplat.com“
 
 public class Ships extends BaseScreen {
 
 
+    private static final int X_POSITION = 600;
+    private static final int Y_POSITION = 290;
 
     private final MainClient ships;
     private SpriteBatch batch;
     private BitmapFont font;
 
     private Texture blueShip, redShip, greenship, topdownfighter;
+    private Texture blueShipRoom, redShipRoom, greenshipRoom, topdownfighterRoom;
+    private Ship blue_ship;
 
     private Stage stage;
     private Skin skin;
     private Skin skinButton;
     private TextButton next;
     private TextButton previous;
+    private TextButton showHideRoom;
     int shipNumber = 0;
-    private boolean isChanged;
-
-
+    int openNumber = 0;
     private Sound spaceShipChange;
+    private ArrayList<Ship> shipList;
+    Player player;
+    Ship ship;
+    ShipControllerImpl shipController;
+    boolean isOpen ;
 
     public Ships(MainClient game) {
         super(game);
@@ -52,27 +66,59 @@ public class Ships extends BaseScreen {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.WHITE);
+
         blueShip = new Texture(Gdx.files.internal("Client/core/assets/data/ships/blueships1.png"));
         redShip = new Texture(Gdx.files.internal("Client/core/assets/data/ships/redship.png"));
         greenship = new Texture(Gdx.files.internal("Client/core/assets/data/ships/greenship.png"));
         topdownfighter = new Texture(Gdx.files.internal("Client/core/assets/data/ships/topdownfighter.png"));
 
 
+        blueShipRoom = new Texture(Gdx.files.internal("Client/core/assets/data/ships/blueships1_section.png"));
+        redShipRoom = new Texture(Gdx.files.internal("Client/core/assets/data/ships/redship_section.png"));
+        greenshipRoom = new Texture(Gdx.files.internal("Client/core/assets/data/ships/green_section.png"));
+        topdownfighterRoom = new Texture(Gdx.files.internal("Client/core/assets/data/ships/topdownfighter_section.png"));
+
 
         spaceShipChange = Gdx.audio.newSound(Gdx.files.internal("Client/core/assets/data/music/change.wav"));
         nextButton();
-
         previousButton();
+        showHideRoom();
+
+
 
         stage.addActor(next);
         stage.addActor(previous);
+        stage.addActor(showHideRoom);
 
 
     }
 
+    private void showHideRoom() {
+        showHideRoom = new TextButton("show rooms", skinButton, "small");
+        showHideRoom.setPosition(BaseScreen.WIDTH/2,100);
+        showHideRoom.getLabel().setColor(Color.BLACK);
+      //  isOpen = true;
+
+
+        showHideRoom.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                spaceShipChange.play();
+                openNumber++;
+                if(isOpen){
+                    showHideRoom.setText("show rooms");
+                    isOpen =false;
+                }else {
+                    showHideRoom.setText("hide rooms");
+                    isOpen = true;
+                }
+            }
+        });
+    }
+
+
     private void previousButton() {
-        previous = new TextButton("Previous", skinButton, "small");
-      //  previous.setSize(150, 60);
+        previous = new TextButton("previous", skinButton, "small");
         previous.setPosition(200,620);
         previous.getLabel().setColor(Color.BLACK);
 
@@ -81,16 +127,18 @@ public class Ships extends BaseScreen {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 spaceShipChange.play();
+
                        if(shipNumber < 0){
                            shipNumber = 3;
-                       }else shipNumber --;
+                       }else {
+                           shipNumber--;
+                       }
             }
         });
     }
 
     private void nextButton() {
-        next = new TextButton("Next", skinButton, "small");
-       // next.setSize(150, 60);
+        next = new TextButton("next", skinButton, "small");
         next.setPosition(1600,620);
 
         next.getLabel().setColor(Color.BLACK);
@@ -123,16 +171,28 @@ public class Ships extends BaseScreen {
 
         switch (shipNumber){
             case 0:
-                stage.getBatch().draw(blueShip, 600,290,720,720);
+                stage.getBatch().draw(blueShip, X_POSITION,Y_POSITION,720,720);
+                if(isOpen){
+                    stage.getBatch().draw(blueShipRoom, X_POSITION,Y_POSITION,720,720);
+                }
                 break;
             case 1:
-                stage.getBatch().draw(redShip,600,290,650,650);
+                stage.getBatch().draw(redShip,X_POSITION,Y_POSITION,720,720);
+                if(isOpen){
+                    stage.getBatch().draw(redShipRoom, X_POSITION,Y_POSITION,720,720);
+                }
                 break;
             case 2:
-                stage.getBatch().draw(greenship,600,290,720,720);
+                stage.getBatch().draw(greenship,X_POSITION,Y_POSITION,720,720);
+                if(isOpen){
+                    stage.getBatch().draw(greenshipRoom, X_POSITION,Y_POSITION,720,720);
+                }
                 break;
             case 3:
-                stage.getBatch().draw(topdownfighter,600,290,720,720);
+                stage.getBatch().draw(topdownfighter,X_POSITION,Y_POSITION,720,720);
+                if(isOpen){
+                    stage.getBatch().draw(topdownfighterRoom, X_POSITION,Y_POSITION,720,720);
+                }
                 break;
 
         }
@@ -167,5 +227,6 @@ public class Ships extends BaseScreen {
         super.dispose();
         skinButton.dispose();
         spaceShipChange.dispose();
+        stage.dispose();
     }
 }
