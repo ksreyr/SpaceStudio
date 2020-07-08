014239837881dffe362a3e3a998dfcf9ecabde0a
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-07-08 23:30:27 +0200

    Updated..!

--- Client/core/src/de/spaceStudio/screens/Options.java
+++ Client/core/src/de/spaceStudio/screens/Options.java
@@ -54,7 +54,7 @@ import de.spaceStudio.util.GdxUtils;
         public Options(MainClient mainClient) {
             this.universeMap = mainClient;
             this.mainClient = mainClient;
-            assetManager = universeMap.getAssetmanager();
+            assetManager = universeMap.getAssetManager();
         }
 
         @Override
