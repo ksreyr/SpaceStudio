38b4da4735b054bf6e27ef268fc80064a4090c70
Liam <liam@contra-bit.com>
2020-07-14 21:19:24 +0200

    Event Manager

--- Client/core/src/de/spaceStudio/screens/StopScreen.java
+++ Client/core/src/de/spaceStudio/screens/StopScreen.java
@@ -9,19 +9,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import de.spaceStudio.MainClient;
 import de.spaceStudio.client.util.Global;
-import de.spaceStudio.server.model.Ship;
+import de.spaceStudio.server.model.CrewMember;
 import de.spaceStudio.server.model.Weapon;
 
 import java.util.Random;
-import java.util.stream.Collectors;
 
 
-public class StopScreen  extends ScreenAdapter {
+public class StopScreen extends ScreenAdapter {
     MainClient game;
-
+    boolean enemyNearBy = true;
     private Stage stage;
     private Skin skin;
-    boolean enemyNearBy = true;
 
     public StopScreen(MainClient game) {
         super();
@@ -40,23 +38,23 @@ public class StopScreen  extends ScreenAdapter {
         return r.nextInt((max - min) + 1) + min;
     }
 
-    String event_description(int event){
+    String event_description(int event) {
         String result;
         switch (event) {
             case 0:
-        int life = Global.currentShip.getHp() + getRandomNumberInRange(1, 50);
+                int life = Global.currentShip.getHp() + getRandomNumberInRange(1, 50);
                 Global.currentShip.setHp(life);
                 result = "You got lucky. You find dearly needed Spare Parts. Current HP = " + life;
                 break;
 
 
             case 1:
-                result = "You meet an abandoned Ship, you find a strange Weapon";
+                int n = 0;
                 int weapon_dammage = Global.currentShip.getHp() + getRandomNumberInRange(1, 50);
-                Global.weaponListPlayer.get(0).
-
-                // TODO SetScreen Fight
-
+                Weapon wepaon = Global.weaponListPlayer.get(n);
+                wepaon.setDamage(weapon_dammage);
+                Global.weaponListPlayer.set(n, wepaon);
+                result = "You meet an abandoned Ship, you find a strange Weapon";
                 break;
 
             case 2:
@@ -69,14 +67,23 @@ public class StopScreen  extends ScreenAdapter {
             case 3:
                 int shield = Global.currentShip.getShield() + getRandomNumberInRange(1, 50);
                 Global.currentShip.setShield(shield);
-                result = "At the abandoned Space Dock there is a Big Box. \n It contains an extra Shield\nShield = " + shield ;
+                result = "At the abandoned Space Dock there is a Big Box. \n It contains an extra Shield\nShield = " + shield;
                 break;
 
             case 4:
                 int shieldDammage = Global.currentShip.getShield() - getRandomNumberInRange(1, 50);
                 Global.currentShip.setShield(shieldDammage);
-                result = "You approach the Station. \n A bomb explodes directly next to the Cockpit. \nShield = " + shieldDammage ;
+                result = "You approach the Station. \n A bomb explodes directly next to the Cockpit. \nShield = " + shieldDammage;
                 break;
+
+            case 5:
+                int health = getRandomNumberInRange(10, 100);
+                CrewMember c = new CrewMember();
+                c.setHealth(health);
+                //c.getImg() // FIXME which List
+                c.setName("Hubert"); // TODO Liam add faker
+                c.setCurrentSection(Global.sectionPlayerList.get(0));
+                // FIXME update Backend
 
             default:
                 result = "Nothing happens. You glance at the vast expanse of Space";
@@ -88,16 +95,15 @@ public class StopScreen  extends ScreenAdapter {
     }
 
 
-
-    String stop_descripton(int event){
+    String stop_descripton(int event) {
         String result;
         switch (event) {
             case 0:
-                result = "In front of you there is just baren Landscape. \n Has there been any Life here?" ;
+                result = "In front of you there is just baren Landscape. \n Has there been any Life here?";
                 break;
 
             case 1:
-                result  = "Before you lies a Blueg Laggon, It looks like an oasis";
+                result = "Before you lies a Blueg Laggon, It looks like an oasis";
                 break;
 
             case 2:
@@ -114,15 +120,26 @@ public class StopScreen  extends ScreenAdapter {
     }
 
 
+    /**
+     * Random events decide, what will haben by loading the event Methods  and setting global Values
+     * A terminal loads a new Screen
+     * Capitals are non Terminals, non Capitals are not
+     * Leave = A
+     * A =  | flee | fight | shop
+     * Flee = stationMap
+     * Shop = enter | exit
+     * Fight = enemy | noEnemy
+     * Stay = stationMap
+     */
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage = new Stage(new FillViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight())));
         skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 
-        final int number = getRandomNumberInRange(0,3);
+        final int number = getRandomNumberInRange(0, 3);
 
 
-        new Dialog("You have a arrived at a new Stop" , skin) {
+        new Dialog("You have a arrived at a new Stop", skin) {
 
             {
                 text(stop_descripton(number));
@@ -141,22 +158,73 @@ public class StopScreen  extends ScreenAdapter {
                             text(outcome);
                             button("Flee", 1l);
                             button("Fight", 2l);
-                            button("Gather", 3l);
+                            button("Shop", 3l);
+
                         }
 
                         @Override
                         protected void result(Object object) {
-                            if (object.equals(2l)) {
-                                // TODO Set FightScreen
-                                game.setScreen(new CombatScreen(game));
-                                // IF there is an enemy
 
-                            } else  if (object.equals(1l)){
+                            if (object.equals(3l)) {
+                                new Dialog("Go to Shop", skin) {
+
+                                    {
+                                        text("Do you want to go to the Shop");
+                                        button("Enter Shop", 1l).getButtonTable().row();
+                                        button("NO", 0l);
+
+                                    }
+
+                                    @Override
+                                    protected void result(Object object) {
+                                        if (object.equals("FIXME")) {
+                                            //   game.setScreen(new (...); // FIXME go to Shop
+                                        }
+                                    }
+                                }.show(stage);
+
+                            } else if (object.equals(2l)) {
+                                // TODO Set FightScreen
+                                if (!enemyNearBy) { // FIXME no enemy
+                                    // IF there is an enemy
+                                    new Dialog("On the Way Back you Stop...", skin) {
+
+                                        {
+                                            text("It has been a long Day You are Tired");
+                                            button("View Map", 1l).getButtonTable().row();
+
+                                        }
+
+                                        @Override
+                                        protected void result(Object object) {
+                                            game.setScreen(new StationsMap(game));
+                                        }
+
+                                    }.show(stage);
+
+                                } else {
+                                    new Dialog("Enemy Ship approaches", skin) {
+
+                                        {
+                                            text("Your radar has picked up an unknown ship on its radar");
+                                            button("Fight");
+                                        }
+
+                                        @Override
+                                        protected void result(Object object) {
+                                            game.setScreen(new CombatScreen(game));
+                                        }
+                                    }.show(stage);
+
+                                }
+
+                            } else if (object.equals(1l)) {
                                 new Dialog("Go back to Map", skin) {
 
                                     {
                                         text("You have seen Enough. You go back to your Cockpit");
                                         button("View Map", 1l).getButtonTable().row();
+
                                     }
 
                                     @Override
@@ -168,14 +236,26 @@ public class StopScreen  extends ScreenAdapter {
 
                         }
                     }.show(stage);
-                } else  if (object.toString() == "false") {
-                    game.setScreen(new StationsMap(game));
+                } else if (object.toString() == "false") {
+                    new Dialog("Go back to Map", skin) {
+
+                        {
+                            text("You have seen Enough. You go back to your Cockpit");
+                            button("View Map", 1l).getButtonTable().row();
+
+                        }
+
+                        @Override
+                        protected void result(Object object) {
+                            game.setScreen(new StationsMap(game));
+                        }
+                    }.show(stage);
                 }
 
-
             }
-            }.show(stage);
+        }.show(stage);
     }
+
 
     @Override
     public void resize(int width, int height) {
