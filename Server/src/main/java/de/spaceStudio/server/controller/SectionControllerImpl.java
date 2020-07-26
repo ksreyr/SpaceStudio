15147e5ec13428afac11b36d4cd50196e9dc7ff2
Liam Hurwitz <hurwitz@uni-bremen.de>
2020-07-26 15:36:53 +0200

    no more bloody numbers

--- Server/src/main/java/de/spaceStudio/server/controller/SectionControllerImpl.java
+++ Server/src/main/java/de/spaceStudio/server/controller/SectionControllerImpl.java
@@ -73,14 +73,14 @@ public class SectionControllerImpl implements SectionController {
         Player playerToUpdate;
         AI ai;
 
-        if(playerRepository.findByName(section.getShip().getOwner().getName()).isPresent()){
-            playerToUpdate=playerRepository.findByName(section.getShip().getOwner().getName()).get();
-            Ship ship = shipRepository.findShipByNameAndAndOwner(section.getShip().getName(),playerToUpdate).get();
+        if (playerRepository.findByName(section.getShip().getOwner().getName()).isPresent()) {
+            playerToUpdate = playerRepository.findByName(section.getShip().getOwner().getName()).get();
+            Ship ship = shipRepository.findShipByNameAndAndOwner(section.getShip().getName(), playerToUpdate).get();
             ship.setOwner(playerToUpdate);
             section.setShip(ship);
-        }else{
-            ai=aiRepository.findByName(section.getShip().getOwner().getName()).get();
-            Ship ship = shipRepository.findShipByNameAndAndOwner(section.getShip().getName(),ai).get();
+        } else {
+            ai = aiRepository.findByName(section.getShip().getOwner().getName()).get();
+            Ship ship = shipRepository.findShipByNameAndAndOwner(section.getShip().getName(), ai).get();
             ship.setOwner(ai);
             section.setShip(ship);
         }
@@ -92,16 +92,17 @@ public class SectionControllerImpl implements SectionController {
     @RequestMapping(value = "/sectiontoadd", method = RequestMethod.POST)
     public String addSections(@RequestBody ArrayList<Section> sections) {
 
-        List<Section> sectionsadded= new ArrayList<>();
+        List<Section> sectionsadded = new ArrayList<>();
         for (Section s :
                 sections) {
-            Section section= repository.save(s);
+            Section section = repository.save(s);
             sectionsadded.add(section);
         }
         Gson gson = new Gson();
         gson.toJson(sectionsadded);
         return gson.toJson(sectionsadded);
     }
+
     /**
      * Update data of the section
      *
@@ -157,18 +158,26 @@ public class SectionControllerImpl implements SectionController {
                     case FIGHTER:
                         Optional<List<Weapon>> weapons = weaponRepository.findBySection(s);
                         if (weapons.isPresent()) { // More Power is Crew is in Sections
-                            s.setPowerCurrent(s.getPowerCurrent() + 1);
+                            int powerBuffFighter = s.getPowerCurrent() + 1;
+                            s.setPowerCurrent(powerBuffFighter);
+                            if (weapons.isPresent()) {
+                                for (Weapon w :
+                                        weapons.get()) {
+                                    int damage = w.getDamage() + 10;
+                                    w.setDamage(damage);
+                                }
+                            }
                         }
                     case TECHNICIAN:
-                        s.setHulleIntegritat(s.getHulleIntegritat() + 5);
+                        int powerBuffTechnician = s.getPowerCurrent() + 10;
+                        s.setPowerCurrent(powerBuffTechnician);
                 }
 
             }
             if (crew.isPresent()) {
                 s.setUsable(true);
             }
-            if(crew.isPresent()&&s.getOxygen()<30)
-            {
+            if (crew.isPresent() && s.getOxygen() < 30) {
                 crewMemberRepository.delete(crew.get());
             }
             sectionRepository.save(s);
